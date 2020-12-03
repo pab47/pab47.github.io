6eaa60c90b8d5f73966dbc9da46c66d729b5ef38
Pranav Bhounsule <pranavab@gmail.com>
2020-12-03 11:44:45 -0600

    Added project pdf and files

---
+++ robotics/f20/pong_basic.m
@@ -0,0 +1,146 @@
+function pong_basic
+%Modified the pong code by David Buckingham
+%https://www.mathworks.com/matlabcentral/fileexchange/31177-dave-s-matlab-pong
+
+%%%%%% main part of the code %%%
+global game_over
+
+close all
+initData  %first function, initialize the data variables
+initFigure %second function, initialize the figure
+while ~game_over %runs till game_over = 1
+    moveBall; %third function, compute ball movement including collision detection
+    movePaddle; %fourth function, compute paddle position based on user input. 
+    refreshPlot; %fifth function, refresh plot based on moveBall and movePaddle
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function initData %first function, initialize the data variables
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global WALL_X_MIN WALL_X_MAX WALL_Y_MIN WALL_Y_MAX
+global PADDLE_WIDTH BALL_SIZE 
+global BALL_INIT_VX BALL_INIT_VY DT DELAY
+global PADDLE_VX PADDLE_SPEED 
+global game_over level
+
+level = 0;
+game_over = 0;
+WALL_X_MIN = 0;
+WALL_X_MAX = 100;
+WALL_Y_MIN = 0;
+WALL_Y_MAX = 100;
+PADDLE_WIDTH = 20;
+BALL_SIZE = 10;
+BALL_INIT_VX = -4;
+BALL_INIT_VY = -4;
+PADDLE_VX = 0;
+PADDLE_SPEED = 5; 
+DT = 0.1; 
+DELAY = 0.001;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function initFigure %second function, initialize the figure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global PADDLE_WIDTH BALL_SIZE 
+global WALL_X_MIN WALL_X_MAX WALL_Y_MIN WALL_Y_MAX
+global BALL_INIT_VX BALL_INIT_VY
+global ball_x ball_y ball_vx ball_vy 
+global paddle_x_left paddle_x_right paddle_y
+global ball_anim paddle_anim
+global level
+
+
+fig = figure; %initialize the figure
+set(fig, 'Resize', 'off'); %do not allow figure to resize
+set(fig,'KeyPressFcn',@keyDown); %setkey presses for later
+ball_x = WALL_X_MAX; %0.5*(WALL_X_MIN+WALL_X_MAX); 
+ball_y = WALL_Y_MAX;
+ball_vx = BALL_INIT_VX; 
+ball_vy = BALL_INIT_VY;
+paddle_x_left = 0;
+paddle_x_right = PADDLE_WIDTH;
+paddle_y = 10;
+axis([WALL_X_MIN WALL_X_MAX WALL_Y_MIN WALL_Y_MAX]); %set the size of the board.
+axis manual;
+hold on;
+title(['press keys <left/right> to move the paddle; level = ',num2str(level)],'Fontsize',14);
+set(gca, 'color', 'w', 'YTick', [], 'XTick', []); %remove x and y label
+
+%%%%%%% set the ball and paddle %%%%%%
+ball_anim = plot(ball_x,ball_y,'o','Markersize',BALL_SIZE,'Markerfacecolor','r','Markeredgecolor','r'); %create ball
+paddle_anim = line('Xdata',[paddle_x_left paddle_x_right],'Ydata',[paddle_y paddle_y],'Color','g','Linewidth',5); %paddle
+
+%%%%%%%% set the walls %%%%%%%
+line('Xdata',[WALL_X_MIN WALL_X_MIN],'Ydata',[WALL_Y_MIN WALL_Y_MAX],'Color','k','Linewidth',3); %left wall
+line('Xdata',[WALL_X_MIN WALL_X_MAX],'Ydata',[WALL_Y_MAX WALL_Y_MAX],'Color','k','Linewidth',3); %top wall
+line('Xdata',[WALL_X_MAX WALL_X_MAX],'Ydata',[WALL_Y_MIN WALL_Y_MAX],'Color','k','Linewidth',3); %right wall
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function moveBall %third function, compute ball movement including collision detection
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global ball_x ball_y ball_vx ball_vy  DT
+global WALL_X_MIN WALL_X_MAX WALL_Y_MIN WALL_Y_MAX
+global paddle_x_left paddle_x_right paddle_y
+global game_over level
+
+ball_x = ball_x + ball_vx*DT;
+ball_y = ball_y + ball_vy*DT;
+
+%%%%%%%% Basic pong edit #1: %%%%%%%%%%%%
+% Write code to move the ball when it touches the padddle/wall over here. 
+% Your code will include logic to get the ball to 
+% detect the left, right, and top wall and the paddle and subsequently to change its
+% movement based on which wall/paddle the ball hits.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if (ball_y < WALL_Y_MIN)
+     game_over = 1;
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function movePaddle %fourth function, compute paddle position based on user input. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global PADDLE_VX 
+global PADDLE_WIDTH DT
+global paddle_x_left paddle_x_right
+global WALL_X_MIN WALL_X_MAX
+
+paddle_x_left = paddle_x_left + PADDLE_VX*DT;
+paddle_x_right = paddle_x_left + PADDLE_WIDTH; 
+
+%%%%%%%% Basic pong edit #2: %%%%%%%%%%%%
+% Write code to move the paddle here. Your code will include logic to get
+% the paddle to detect the left and right wall and to avoid it from
+% penetrating either walls. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function refreshPlot %fifth function, refresh plot based on moveBall and movePaddle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global ball_x ball_y
+global ball_anim paddle_anim
+global paddle_x_left paddle_x_right paddle_y
+global DELAY
+
+set(ball_anim, 'XData', ball_x, 'YData', ball_y);
+set(paddle_anim, 'Xdata',[paddle_x_left paddle_x_right], 'YData', [paddle_y paddle_y]);
+drawnow;
+pause(DELAY);   
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function keyDown(src,event) %called from initFigure to take key press to move the paddle
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+global PADDLE_VX PADDLE_SPEED
+
+switch event.Key
+  case 'rightarrow'
+    PADDLE_VX = PADDLE_SPEED;
+  case 'leftarrow'
+    PADDLE_VX = -PADDLE_SPEED;
+  otherwise
+    PADDLE_VX = 0;
+end
+
