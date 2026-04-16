1531c28942a130207f25e39b20c57822ae834851
Pranav Bhounsule <20406673+pab47@users.noreply.github.com>
2026-04-16 13:32:39 -0500

    Delete info/tello_emergency.py

--- info/tello_emergency.py
+++
@@ -1,8 +0,0 @@
-import sys
-#sys.path.append(r'c:\users\giuseppe\appdata\local\packages\pythonsoftwarefoundation.python.3.10_qbz5n2kfra8p0\localcache\local-packages\python310\site-packages')
-
-from djitellopy import Tello
-
-tello = Tello()
-tello.connect()
-tello.land()
